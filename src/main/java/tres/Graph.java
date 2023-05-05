81c8c0ce70d74ccfe1658c86dff783fe99372f55
rubencerroo <ruben.cerrodiaz@usp.ceu.es>
2023-05-05 10:12:17 +0200

    JavaDoc

--- src/main/java/tres/Graph.java
+++ src/main/java/tres/Graph.java
@@ -2,13 +2,19 @@ package tres;
 
 import java.util.*;
 
+/***********************
+* Practica 3 -
+* @author rubencerroo 
+* @version 1.0
+************************/
 public class Graph<V> {
+    /* Metod Main (Opcional) */
     public static void main(String[] args) {
         Graph<Integer> graph = new Graph<Integer>();
         graph.addVertex(1);
         graph.addVertex(2);
         graph.addEdge(1, 2);
-    
+
         try {
             Set<Integer> adjacents = graph.obtainAdjacents(1);
             System.out.println(adjacents);
@@ -16,9 +22,17 @@ public class Graph<V> {
             e.printStackTrace();
         }
     }
-    
+
+    // Lista de adyacencia.
     private Map<V, Set<V>> adjacencyList = new HashMap<>();
 
+    /******************************************************************
+     * Añade el vértice ‘v‘ al grafo.
+     *
+     * @param v vértice a añadir.
+     * @return ‘true‘ si no estaba anteriormente y ‘false‘ en caso
+     *         contrario.
+     ******************************************************************/
     public boolean addVertex(V v) {
         if (adjacencyList.containsKey(v)) {
             return false;
@@ -26,6 +40,17 @@ public class Graph<V> {
         adjacencyList.put(v, new HashSet<V>());
         return true;
     }
+
+    /******************************************************************
+     * Añade un arco entre los vértices ‘v1‘ y ‘v2‘ al grafo. En
+     * caso de que no exista alguno de los vértices, lo añade
+     * también.
+     *
+     * @param v1 el origen del arco.
+     * @param v2 el destino del arco.
+     * @return ‘true‘ si no existía el arco y ‘false‘ en caso
+     *         contrario.
+     ******************************************************************/
 
     public boolean addEdge(V v1, V v2) {
         if (!adjacencyList.containsKey(v1)) {
@@ -37,6 +62,12 @@ public class Graph<V> {
         return adjacencyList.get(v1).add(v2);
     }
 
+    /******************************************************************
+     * Obtiene el conjunto de vértices adyacentes a ‘v‘.
+     *
+     * @param v vértice del que se obtienen los adyacentes.
+     * @return conjunto de vértices adyacentes.
+     ******************************************************************/
     public Set<V> obtainAdjacents(V v) throws Exception {
         if (!adjacencyList.containsKey(v)) {
             throw new Exception("Vertex not found in graph");
@@ -44,10 +75,22 @@ public class Graph<V> {
         return adjacencyList.get(v);
     }
 
+    /******************************************************************
+     * Comprueba si el grafo contiene el vértice dado.
+     *
+     * @param v vértice para el que se realiza la comprobación.
+     * @return ‘true‘ si ‘v‘ es un vértice del grafo.
+     ******************************************************************/
     public boolean containsVertex(V v) {
         return adjacencyList.containsKey(v);
     }
 
+    /******************************************************************
+     * Método ‘toString()‘ reescrito para la clase ‘Grafo.java‘.
+     * 
+     * @return una cadena de caracteres con la lista de
+     *         adyacencia.
+     ******************************************************************/
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -60,7 +103,17 @@ public class Graph<V> {
         }
         return sb.toString();
     }
-    
+
+    /*********************************************************
+     * Obtiene, en caso de que exista, un camino entre ‘v1‘ y
+     * ‘v2‘. En caso contrario, devuelve ‘null‘.
+     *
+     * @param v1 el vértice origen.
+     * @param v2 el vértice destino.
+     * @return lista con la secuencia de vértices desde ‘v1‘ hasta
+     *         ‘v2‘ * pasando por arcos del grafo.
+     *********************************************************/
+
     public List<V> onePath(V v1, V v2) {
         Map<V, V> trace = new HashMap<>();
         Deque<V> open = new ArrayDeque<>();
--- src/test/java/tres/GraphTest.java
+++ src/test/java/tres/GraphTest.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GraphTest {
-    
+
     /**
      * Este test comprueba que el método ‘onePath(V v1, V v2)‘
      * encuentra un camino entre ‘v1‘ y ‘v2‘ cuando existe.
      */
     @Test
-    public void onePathFindsAPath(){
+    public void onePathFindsAPath() {
         System.out.println("\nTest onePathFindsAPath");
         System.out.println("----------------------");
         // Se construye el grafo.
@@ -29,7 +29,7 @@ public class GraphTest {
         expectedPath.add(5);
         expectedPath.add(6);
         expectedPath.add(4);
-        //Se comprueba si el camino devuelto es igual al esperado.
+        // Se comprueba si el camino devuelto es igual al esperado.
         assertEquals(expectedPath, g.onePath(1, 4));
         System.out.println(g);
     }
